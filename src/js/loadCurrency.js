df85c6ab3154285eef8d9e3ea780d4515c1bbf67
Nikita <oficialsmashgame@gmail.com>
2022-12-17 17:31:21 +0300

    render fixes + restyling some components

--- src/js/loadCurrency.js
+++ src/js/loadCurrency.js
@@ -1,14 +1,23 @@
 function loadCurrency(toCurrency) {
-    const currencyTags = document.querySelectorAll('.exchange__currency-text')
+    const wrapper = document.querySelector('.exchange__currency')
 
-    function getCurrency(tags) {
-        tags.forEach(async (tag) => {
-            const currencyName = tag.textContent.split(':')[0]
+    const currencyList = ['USD', 'EUR', 'SGD', 'JPY', 'CNH', 'HKD']
+
+    function render(currency, exchange) {
+        return `
+            <div class="exchange__currency-block">
+                <div class="exchange__currency-text">${currency}:&nbsp;</div>
+                <div class="exchange__currency-amount">${typeof exchange === 'number' ? exchange.toFixed(2) : 'N/A'}</div>
+            </div>
+        `
+    }
+
+    function getCurrency(currencies, toCurrency) {
+        currencies.forEach(async (currency) => {
             let currencyExchange = 0
 
-            currencyExchange = await getOneCurrency(currencyName, toCurrency)
-
-            tag.nextElementSibling.innerText = currencyExchange.toFixed(2)
+            currencyExchange = await getOneCurrency(currency, toCurrency)
+            wrapper.innerHTML += render(currency, currencyExchange)
         })
     }
 
@@ -28,11 +37,11 @@ function loadCurrency(toCurrency) {
             .then(response => {
                 currencyExchange = response
             })
-            .catch(err => console.error(err));
+            .catch(error => console.error(error));
 
         return currencyExchange
     }
 
-    getCurrency(currencyTags, toCurrency)
+    getCurrency(currencyList, toCurrency)
 }
 
--- src/js/loadNews.js
+++ src/js/loadNews.js
@@ -1,4 +1,5 @@
 async function loadNews() {
+    const wrapper = document.querySelector('.news__items')
     const mockImgSrc = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTNemDx32kHbtVSXNMkRyINamV255JkT2r347HXiCI7CIFk3afiF0dG8VmQOkLTuPzprM&usqp=CAU'
 
     function cutText(text, limit) {
@@ -20,48 +21,45 @@ async function loadNews() {
         return text;
     }
 
+    function render(article = false) {
+        let img = mockImgSrc
+        let url = 'javascript:void'
+        let title = 'Не удалось загрузить новости!<br>Попробуйте перезагрузить страницу'
+        let description = ''
+        
+        const status = Boolean(article)
+
+        if (status) {
+            img = article?.urlToImage ? article.urlToImage : mockImgSrc
+            title = cutText(article?.title, 60)
+            description = cutText(article?.description, 100)
+            url = article.url
+        }
+
+        return `
+        <div class="news__link">
+            <div class="news__item">
+                    <div class="news__item-img">
+                        <img class="news__img" src="${img}" alt="newsImg"">
+                    </div>
+                    <h4 class="news__item-title">
+                        ${title}
+                    </h4>
+                    <p class="news__item-text">
+                        <a class="${status ? 'news__item-link' : 'news__item-message'}" href=${url} target="_blank">
+                            ${description}
+                        </a>
+                    </p>
+            </div>
+        </div>
+        `
+    }
+
     await fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b8e220bebe8e422fa69d8f4844730fec')
         .then(response => response.json())
         .then(response => {
             for (let i = 0; i <= response.articles.length - 1; i++) {
-                const wrapper = document.querySelector('.news__items')
-
-                try {
-                    const article = response.articles[i]
-
-                    wrapper.innerHTML += `
-                    <div class="news__link">
-                        <div class="news__item">
-                                <div class="news__item-img">
-                                    <img class="news__img" src="${article?.urlToImage ? article.urlToImage : mockImgSrc}" alt="newsImg"">
-                                </div>
-                                <h4 class="news__item-title">
-                                    ${cutText(article?.title, 60)}
-                                </h4>
-                                <p class="news__item-text">
-                                    <a class="news_item-link" href=${article.url}>
-                                        ${cutText(article?.description, 100)}
-                                    </a>
-                                </p>
-                        </div>
-                    </div>
-                    `
-                } catch (error) {
-                    wrapper.innerHTML += `
-                        <div class="news__item">
-                            <div class="news__item-img">
-                            <img class="news__img" src=${mockImgSrc} alt="newsImg">
-                            </div>
-                            <h4 class="news__item-title">
-                                Не удалось загрузить новости!
-                            </h4>
-                            <p class="news__item-text">
-                                Попробуйте перезагрузить страницу
-                            </p>
-                        </div>
-                    `
-                    throw new Error(error)
-                }
+                wrapper.innerHTML += render(response.articles[i])
             }
 
             const imgs = document.querySelectorAll('.news__img')
@@ -70,6 +68,10 @@ async function loadNews() {
                     img.src = mockImgSrc
                 }
             })
+        })
+        .catch(error => {
+            wrapper.innerHTML += render()
+            throw new Error(error)
         })
 
     newsSlider()
