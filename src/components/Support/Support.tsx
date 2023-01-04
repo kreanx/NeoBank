0ecdfa995e753d59a30486e543f32f0aa750ad78
Nikita <oficialsmashgame@gmail.com>
2023-01-04 16:27:29 +0300

    added email POST request in support block

--- src/components/Support/Support.tsx
+++ src/components/Support/Support.tsx
@@ -2,8 +2,47 @@ import Container from '../Container/Container'
 import styles from './Support.module.scss'
 import planeImg from '../../img/icons/plane.png'
 import emailImg from '../../img/icons/email.png'
+import { useState, useEffect } from 'react'
 
 const Support: React.FC = () => {
+	const [value, setValue] = useState('')
+	const [isSubscribed, setSubscribed] = useState(false)
+
+	useEffect(() => {
+		if (localStorage.getItem('isSubscribed')) {
+			setSubscribed(true)
+		}
+	}, [])
+
+	const sendEmail = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+
+		await fetch('/email', {
+			method: 'POST',
+			body: JSON.stringify(value),
+		})
+			.then((response) => {
+				if (!response.ok) {
+					return Promise.reject('Failed to send request!')
+				}
+				response.json()
+			})
+			.then((data) => {
+				window.localStorage.setItem('isSubscribed', 'true')
+				setSubscribed(true)
+				console.log('You are subscribed:', data)
+			})
+			.catch((error) => {
+				setValue('')
+				console.error('Error:', error)
+				alert('Failed to send request!')
+			})
+	}
+
+	const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setValue(e.target.value)
+	}
+
 	return (
 		<section className={styles.support}>
 			<Container>
@@ -13,25 +52,31 @@ const Support: React.FC = () => {
 						Subscribe Newsletter &amp;&nbsp;get
 					</h3>
 					<h4 className={styles.support__info}>Bank News</h4>
-					<form className={styles.support__input}>
+					<form className={styles.support__input} onSubmit={sendEmail}>
 						<img src={emailImg} alt="email" />
-						<span className={styles.support__subscribed}>
-							You are already subscribed!
-						</span>
-						<input
-							type="email"
-							className={styles.support__input_input}
-							placeholder="Your email"
-						/>
-						<button className={styles.support__button} type="submit">
-							<img
-								className={styles.support__button_img}
-								src={planeImg}
-								alt="plane"
-							/>
-
-							<span className={styles.support__button_text}>Subscribe</span>
-						</button>
+						{!isSubscribed ? (
+							<>
+								<input
+									type="email"
+									className={styles.support__input_input}
+									placeholder="Your email"
+									onInput={inputHandler}
+									value={value}
+								/>
+								<button className={styles.support__button} type="submit">
+									<img
+										className={styles.support__button_img}
+										src={planeImg}
+										alt="plane"
+									/>
+									<span className={styles.support__button_text}>Subscribe</span>
+								</button>
+							</>
+						) : (
+							<span className={styles.support__subscribed}>
+								You are already subscribed!
+							</span>
+						)}
 					</form>
 				</div>
 			</Container>
