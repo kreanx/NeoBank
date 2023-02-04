69e2f128262a0478ae7363e1767941dce03e6ddc
Nikita <oficialsmashgame@gmail.com>
2023-02-05 00:10:04 +0300

    added CC_DENIED check function in stepComplete

--- src/components/StepComplete/StepComplete.tsx
+++ src/components/StepComplete/StepComplete.tsx
@@ -1,12 +1,41 @@
 import styles from './StepComplete.module.scss'
 import Container from 'ui-kit/Container/Container'
+import { useEffect } from 'react'
+import { getApplicationStatus } from 'services/api/api'
+import { mainDefaultStep } from 'store/slices/mainStepSlice'
+import localStorageHandler from 'services/localStorage/localStorageHandler'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useAppDispatch } from 'hook'
+import { routes } from 'services/routes'
 
 interface IStepComplete {
 	title: string
 	subtitle: string
+	isCheck?: boolean
 }
 
-const StepComplete: React.FC<IStepComplete> = ({ title, subtitle }) => {
+const StepComplete: React.FC<IStepComplete> = ({
+	title,
+	subtitle,
+	isCheck,
+}) => {
+	const applicationId = useParams()
+	const dispatch = useAppDispatch()
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		if (isCheck) {
+			setTimeout(async () => {
+				const status = await getApplicationStatus(applicationId.applicationId)
+				if (status?.status === 'CC_DENIED') {
+					dispatch(mainDefaultStep())
+					localStorageHandler('application', 'remove')
+					navigate(routes.home)
+				}
+			}, 10000)
+		}
+	}, [])
+
 	return (
 		<div className={styles.stepComplete}>
 			<Container>
